29b8a859fdf745a772236651b9b58523b64295e5
Rodion <markov-r@mail.ua>
2019-08-22 13:36:35 +0300

    Add AngularJS

---
+++ js/angularjs/clothes-ctrl.js
@@ -0,0 +1,6 @@
+app.controller('ClothesCtrl',['$scope', function($scope) {
+    $scope.man = manClothes;
+    $scope.woman;
+    $scope.cildren;
+    $scope.all;
+}]);
---
+++ js/data-clothes/clothes.js
@@ -0,0 +1,42 @@
+var manClothes = [{
+        id: 1,
+        name: "Bodysuit",
+        size: [48,50,56],
+        categori: top,
+        price:25
+    },
+    {
+        id: 2,
+        name: "Booties",
+        size: [48,50,56],
+        categori: top,
+        price:28
+    },
+    {
+        id: 3,
+        name: "Romper",
+        size: [48,50,56],
+        categori: top,
+        price:29
+    },
+    {
+        id: 4,
+        name: "Cloth diaper",
+        size: [48,50,56],
+        categori: top,
+        price:15
+    },
+    {
+        id: 5,
+        name: "Bib",
+        size: [48,50,56],
+        categori: top,
+        price:43
+    },
+    {
+        id: 6,
+        name: "Onesie",
+        size: [48,50,56],
+        categori: top,
+        price:61
+    } ];
